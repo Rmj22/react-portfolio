28ab6b5c8a9d12e190b07f524bb9f68833816b1f
Robin Jones <RobinJones335@gmail.com>
2020-04-16 21:44:21 -0400

    added new background

--- profile-app/src/components/navbar/index.js
+++ profile-app/src/components/navbar/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import '../navbar/style.css'
+// import '../navbar/style.css'
 // import {faGithub, falistalt,facouch,fabriefcase } from'@fontawesome'
+// import FontAwesome from 'react-fontawesome'
 
 
 
@@ -10,15 +11,15 @@ import '../navbar/style.css'
 export class TopNav extends Component {
   render() {
     return (
-      <div id="nav" >
-        <Navbar>
-          <Navbar.Brand href="#home">React Portfolio Site</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="/" >Home   </Nav.Link>
-            <Nav.Link href="/Work">Work </Nav.Link>
-            <Nav.Link href="https://www.linkedin.com/in/robin-jones-7534bb167/">linkedin</Nav.Link>
-            <Nav.Link href="/Resume">Resume </Nav.Link>
-            <Nav.Link href="https://github.com/Rmj22">Github</Nav.Link>
+      <div  >
+        <Navbar bg="dark">
+          <Navbar.Brand >React Portfolio Site</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link  href="/" >Home </Nav.Link>
+            <Nav.Link  href="/Work">Work </Nav.Link>
+            <Nav.Link  href="https://www.linkedin.com/in/robin-jones-7534bb167/">linkedin</Nav.Link>
+            <Nav.Link  href="/Resume">Pre-Screen </Nav.Link>
+            <Nav.Link  href="https://github.com/Rmj22">Github</Nav.Link>
           </Nav>
         </Navbar>
       </div>
